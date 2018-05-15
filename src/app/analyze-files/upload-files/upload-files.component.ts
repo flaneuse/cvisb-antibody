a21128ff3fe7fa6a23d4d6267cef14bc81cc7b37
Laura Hughes <flaneuse@users.noreply.github.com>
2018-05-15 14:30:40 -0700

    pass formdata to python backend

--- src/app/analyze-files/upload-files/upload-files.component.ts
+++ src/app/analyze-files/upload-files/upload-files.component.ts
@@ -19,10 +19,10 @@ export class UploadFilesComponent implements OnInit {
   // private progress: number = 0;
   private expt_types = ['ADCD', 'ADCP', 'ADNP', 'NKD']
   private file_types = [
-    {id: 'plates', label: 'plate/sample layout' },
-    {id: 'data', label: 'fluorescence data' },
-    {id: 'raw', label: '.acs file' },
-]
+    { id: 'plates', label: 'plate/sample layout' },
+    { id: 'data', label: 'fluorescence data' },
+    { id: 'raw', label: '.acs file' },
+  ]
 
   // = new FileUploader({ url: 'http://127.0.0.1:5000/upload' });
   URL: string = 'http://127.0.0.1:5000/upload';
@@ -41,8 +41,10 @@ export class UploadFilesComponent implements OnInit {
   ngOnInit() {
     // TODO: formData: data to pass with item.
 
-    this.uploader = new FileUploader({ url: this.URL, authToken: "testToken",
-      autoUpload: false });
+    this.uploader = new FileUploader({
+      url: this.URL, authToken: "testToken",
+      autoUpload: false
+    });
 
     // console.log(this.uploader)
     //
@@ -53,19 +55,42 @@ export class UploadFilesComponent implements OnInit {
     //   this.progress = progress;
     // }
     //
-    this.uploader.onBeforeUploadItem = function(item) {
-    console.log(this.selected_expt)
-      item.formData = [{ firstParam: 'Value', secondParam: 'value' }];
+    // this.uploader.onBeforeUploadItem = function(item) {
+    // console.log(this.selected_expt)
+    //   item.formData = [{ firstParam: 'Value', secondParam: 'value' }];
+    // };
+
+// Solved via https://stackoverflow.com/questions/38502687/how-to-submit-post-data-with-ng2-file-upload-in-angular-2
+    this.uploader.onBuildItemForm = (fileItem: any, form: any) => {
+      console.log('uploader')
+      console.log(fileItem.formData)
+      // console.log(form)
+      // form = fileItem.formData;
+      // form.append('expt', "fileItem.formData['expt']"); //note comma separating key and value
+      //
+      // form = fileItem.formData
+      // return(form)
+      //
+      for (let key of Object.keys(fileItem.formData)) {
+   console.log(key);
+   form.append(key, fileItem.formData[key])
+}
+      // form.append(fileItem.formData)
+      console.log(form)
+      // form.append('someField2', 'this.someValue2');
     };
 
     this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
+      console.log('file completed uploading')
       // this.fileurl = response
-      console.log(item)
-      console.log(status)
-      console.log(headers)
+      // console.log(item)
+      // console.log(status)
+      // console.log(headers)
       // console.log(response)
     };
-    console.log(this.uploader)
+    // console.log(this.uploader)
   }
+
+
 
 }
