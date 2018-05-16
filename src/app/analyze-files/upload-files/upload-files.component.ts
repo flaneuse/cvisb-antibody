d15cdcdd1bee6b04ee1beb00b417c6691f11ff52
Laura Hughes <flaneuse@users.noreply.github.com>
2018-05-16 15:07:58 -0700

    check for file upload

--- src/app/analyze-files/upload-files/upload-files.component.ts
+++ src/app/analyze-files/upload-files/upload-files.component.ts
@@ -19,11 +19,15 @@ export class UploadFilesComponent implements OnInit {
   // private progress: number = 0;
   private expt_types = ['ADCD', 'ADCP', 'ADNP', 'NKD'];
   private file_types = [
-    { id: 'plates', label: 'plate/sample layout', search_string: 'layout' },
-    { id: 'data', label: 'fluorescence data', search_string: 'flowjo' },
-    { id: 'raw', label: '.acs file', search_string: '\.acs' },
+    { id: 'plates', label: 'plate/sample layout', uploaded: false, search_string: 'layout' },
+    { id: 'data', label: 'fluorescence data', uploaded: false, search_string: 'flowjo' },
+    { id: 'raw', label: '.acs file', uploaded: false, search_string: '\.acs' },
   ];
-  private expt_ids: string[] = [];
+
+  private expt_ids: Object[] = [];
+
+  private missing_files: boolean = false;
+
   exptid_pattern: string = "[A-Z]{3}EXP[0-9]+"; // Pattern to search for the experiment ID.
 
   // = new FileUploader({ url: 'http://127.0.0.1:5000/upload' });
@@ -39,7 +43,42 @@ export class UploadFilesComponent implements OnInit {
   constructor(
   ) { }
 
+
+  removeFile(event, item) {
+    item.remove();
+
+    if(this.uploader.queue.length == 0) {
+      this.expt_ids = [];
+      this.missing_files = false;
+    } else {
+      // switch off whether that particular file has been uploaded
+      this.trackUploads(item.file.name);
+    }
+
+
+  }
+
+  clearQueue() {
+    this.uploader.clearQueue();
+    // reset the uploaded files and list of expt ids.
+    this.expt_ids = [];
+    this.missing_files = false;
+
+
+    // let files = this.uploader.queue.map(d => d.file.name);
+    // console.log(this.uploader.queue)
+    //
+    // // Update the file list
+    // this.resetUploaded();
+    //
+    // // Update whether files are missing
+    // this.checkMissing();
+  }
+
+
   ngOnInit() {
+    console.log(this.expt_ids)
+    console.log()
     // TODO: formData: data to pass with item.
 
     this.uploader = new FileUploader({
@@ -62,17 +101,30 @@ export class UploadFilesComponent implements OnInit {
     // };
     //
     this.uploader.onAfterAddingFile = (fileItem: any) => {
+      // Pull out the filename uploaded
       let filename = fileItem.file.name;
+
+      let exptid = this.findExptID(filename);
+      let filetype = this.findFileType(filename);
+
       // Update the params to pass to python.
-
-      let id = this.findExptID(filename);
-      fileItem.formData['expt_id'] = id;
-      id != null && this.expt_ids.indexOf(id) === -1 && this.expt_ids.push(id); // add to the list of expts if not already in the list
-
+      fileItem.formData['expt_id'] = exptid;
+      // id != null && this.expt_ids.indexOf(id) === -1 && this.expt_ids.push(id); // add to the list of expts if not already in the list
       fileItem.formData['expt'] = this.findExptType(filename);
-      fileItem.formData['file'] = this.findFileType(filename);
+      fileItem.formData['file'] = filetype;
 
+      // Keep track of if the right files have been Uploaded
+      // // add to the list of expts if not already in the list
+      // if ID exists, update the upload status of the files
+      this.trackUploads(filename)
+
+
+
+      // Update the count of missing files.
+      this.checkMissing();
+      console.log(this.expt_ids)
     }
+
 
     // Params passing to Flask solved via https://stackoverflow.com/questions/38502687/how-to-submit-post-data-with-ng2-file-upload-in-angular-2
     this.uploader.onBuildItemForm = (fileItem: any, form: any) => {
@@ -91,6 +143,63 @@ export class UploadFilesComponent implements OnInit {
       console.log(response)
     };
     // console.log(this.uploader)
+  }
+
+  checkMissing() {
+    let uploads = this.expt_ids.map(d => d.filetype.map(g => g.uploaded))
+
+    let isMissing = function(val) {
+      return !val;
+    }
+
+    // flatten array
+    uploads = [].concat.apply([], uploads)
+
+    this.missing_files = uploads.some(isMissing);
+  }
+
+  trackUploads(filename) {
+    let exptid = this.findExptID(filename);
+    let filetype = this.findFileType(filename);
+
+    let idx = this.expt_ids.findIndex(d => d.expt_id === exptid);
+
+    // if id doesn't exist in the data frame at all, create as a dictionary
+    idx === -1 ? this.initializeUploaded(exptid, filetype) : this.changeUploaded(idx, filetype);
+  }
+
+  initializeUploaded(exptid, filetype) {
+
+    // NOTE: must use deep copying to create a *copy* not pointer to this.file_types
+
+    let arr = JSON.parse(JSON.stringify(this.file_types));
+    // these copy just the array, but also need to copy the objects WITHIN the array. These methods just provide pointers to the process.
+    // let arr = this.file_types.concat(); // doesn't work
+    // let arr = this.file_types.slice(); // doesn't work
+    // let arr = this.file_types.splice(0); // doesn't work -- removes obj from this.file_types
+    // let arr = [...this.file_types]; // doesn't work.
+    this.expt_ids.push({ 'expt_id': exptid, 'filetype': arr })
+
+    let idx = this.expt_ids.findIndex(d => d.expt_id === exptid);
+
+    this.changeUploaded(idx, filetype);
+
+  }
+
+  changeUploaded(idx, filetype) {
+    let idx_upload = this.expt_ids[idx]['filetype'].findIndex(d => d.id == filetype);
+
+    this.expt_ids[idx]['filetype'][idx_upload]['uploaded'] = !this.expt_ids[idx]['filetype'][idx_upload]['uploaded'];
+  }
+
+  resetUploaded() {
+    for (let i = 0; i < this.expt_ids.length; i++) {
+      let indiv_expt = this.expt_ids[i]['filetype'];
+
+      for (let j = 0; j < indiv_expt.length; j++) {
+        indiv_expt[j]['uploaded'] = false;
+      }
+    }
   }
 
   // Scans filename to set the experiment type
@@ -115,9 +224,9 @@ export class UploadFilesComponent implements OnInit {
   findExptID(filename) {
     let id = filename.match(this.exptid_pattern);
 
-      if(id !== null) {
-        return id[0];
-      }
+    if (id !== null) {
+      return id[0];
+    }
   }
 
 
